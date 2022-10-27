abf42101d584b775a34ad98a90811fa570acbe4c
Mirror <yllikano@gmail.com>
2022-10-27 12:06:04 +0300

    fix

--- src/js/modal.js
+++
@@ -1,7 +0,0 @@
-const modal = (element) => {
-  element.classList.add("modal--active");
-
-  element.querySelector(".button__close").onclick = () => {
-    element.classList.remove("modal--active");
-  };
-};
